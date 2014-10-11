beaccb0398ce3435f5b3c912a3df3682311719ea
stitarsolej <s.titarsolej@gmail.com>
2014-10-11 21:40:24 +0200

    Edited template for GoosePatrol game

--- Boot.js
+++ Boot.js
@@ -1,52 +1,26 @@
-var BasicGame = {};
+var GoosePatrol = {};
 
-BasicGame.Boot = function (game) {
+GoosePatrol.Boot = function (game) {};
 
-};
+GoosePatrol.Boot.prototype = {
+    preload: function() {
+        this.load.image('preloadBar', 'resources/loadbar.gif')
+    },
 
-BasicGame.Boot.prototype = {
-
-    init: function () {
-
-        //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
+    create: function() {
         this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = false;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 800;
+        this.scale.minHeight = 600;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.stage.forcePortrait = true;
+        this.scale.setScreenSize(true);
 
-        //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
-        this.stage.disableVisibilityChange = true;
+        this.input.addPointer();
+        this.stage.backgroundColor = '#000000';
 
-        if (this.game.device.desktop)
-        {
-            //  If you have any desktop specific settings, they can go in here
-            this.scale.pageAlignHorizontally = true;
-        }
-        else
-        {
-            //  Same goes for mobile settings.
-            //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(480, 260, 1024, 768);
-            this.scale.forceLandscape = true;
-            this.scale.pageAlignHorizontally = true;
-            this.scale.setScreenSize(true);
-            this.scale.refresh();
-        }
-
-    },
-
-    preload: function () {
-
-        //  Here we load the assets required for our preloader (in this case a background and a loading bar)
-        this.load.image('preloaderBackground', 'images/preloader_background.jpg');
-        this.load.image('preloaderBar', 'images/preloadr_bar.png');
-
-    },
-
-    create: function () {
-
-        //  By this point the preloader assets have loaded to the cache, we've set the game settings
-        //  So now let's start the real preloader going
         this.state.start('Preloader');
-
     }
-
 };
