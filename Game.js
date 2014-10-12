f9240b309a4006b9e1ddc2fee91a76c03e01bf6c
stitarsolej <s.titarsolej@gmail.com>
2014-10-12 14:36:41 +0200

    Fixed some bugs

--- Game.js
+++ Game.js
@@ -25,9 +25,9 @@ GoosePatrol.Game.prototype = {
 
         for(var i = 0; i < (this.amtOfGoose/2); i++){
             var gLeft = this.gooseGroup.create(this.rnd.integerInRange(-500, -100), 375, 'goose');
-            gLeft.body.velocity.x=150
+            gLeft.body.velocity.x=200;
             var gRight = this.gooseGroup.create(this.rnd.integerInRange(900, 1400), 375, 'goose');
-            gRight.body.velocity.x=-150
+            gRight.body.velocity.x=-200;
 
         }
         gLeft.anchor.setTo(0.5, 0.5);
