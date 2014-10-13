6baa6a9e9e17772a576246cf4c295444a7d3cb5d
stitarsolej <s.titarsolej@gmail.com>
2014-10-13 14:56:06 +0200

    Tried to fix some bugs

--- Game.js
+++ Game.js
@@ -24,11 +24,9 @@ GoosePatrol.Game.prototype = {
         this.gooseGroup.enableBody = true;
 
         for(var i = 0; i < (this.amtOfGoose/2); i++){
-            var gLeft = this.gooseGroup.create(this.rnd.integerInRange(-500, -100), 375, 'goose');
-            gLeft.body.velocity.x=200;
+            var gLeft = this.Goose.spawn(this.rnd.integerInRange(900, 1400), 375, 200);
             var gRight = this.gooseGroup.create(this.rnd.integerInRange(900, 1400), 375, 'goose');
             gRight.body.velocity.x=-200;
-
         }
         gLeft.anchor.setTo(0.5, 0.5);
         gRight.anchor.setTo(0.5, 0.5);
@@ -36,5 +34,26 @@ GoosePatrol.Game.prototype = {
 
     update: function() {
 
+    }
+};
+
+
+GoosePatrol.Goose = {
+    spawn: function(x, y, velocity) {
+        var goose = this.gooseGroup.create(x,y,'goose');
+        goose.inputEnabled = true;
+        //birdy.animations.add('fly', [0,1], 20, true);
+        //birdy.animations.play('fly');
+        goose.body.velocity.x = velocity;
+        return goose;
+        //this.player.body.collideWorldBounds = true;
+    },
+    clickBird: function(bird) {
+        bird.body.velocity.y = 200;
+        bird.body.velocity.x = 0;
+        alert('You hit the bird!')
+    },
+    removeBird: function(bird) {
+        bird.kill()
     }
 };
